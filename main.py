0f6285b45e23fec583d232b869fc3b4d25b02a31
DolmatovaKsenia <fantastic.drogon@gmail.com>
2025-02-20 18:38:38 +0300

    added

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ def main():
     region_map = []
     Region = namedtuple('Region', ['region_code', 'region_name', 'district_name',
                                    'event_city', 'travel_city', 'cityReg'])
-    my_file = open("regions.txt", 'r')
-    directory = "regions_csv"
+    my_file = open("final_regions.txt", 'r')
+    directory = "final_regions_csv"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     while True:
         abracadabra = my_file.readline()
         print(abracadabra)
@@ -20,29 +22,32 @@ def main():
         region_map.append(Region(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]))
         for region in region_map:
             cityReg = region.cityReg
-            data = pd.read_csv("coord2geohash_csv/" + cityReg + ".csv", sep=',')
-            h3all = data["geohash"].values
+            try:
+                data = pd.read_csv("final_csv_dir/" + cityReg + ".csv", sep=',')
+                h3all = data["geohash"].values
 
-            N = len(h3all)
-            region_code_arr = [region.region_code] * N
-            region_name_arr = [region.region_name] * N
-            district_id_arr = [None] * N
-            district_name_arr = [region.district_name] * N
-            geohash_arr = h3all
-            event_city_arr = [region.event_city] * N
-            travel_city_arr = [region.travel_city] * N
-            df = pd.DataFrame({
-                "region_code": region_code_arr,
-                "region_name": region_name_arr,
-                "district_id": district_id_arr,
-                "district_name": district_name_arr,
-                "geohash": geohash_arr,
-                "event_city": event_city_arr,
-                "travel_city": travel_city_arr
-            })
-            file_name = f"geo_ref_{region.district_name}.csv"
-            new_path = os.path.join(directory, file_name)
-            df.to_csv(new_path)
+                N = len(h3all)
+                region_code_arr = [region.region_code] * N
+                region_name_arr = [region.region_name] * N
+                district_id_arr = [None] * N
+                district_name_arr = [region.district_name] * N
+                geohash_arr = h3all
+                event_city_arr = [region.event_city] * N
+                travel_city_arr = [region.travel_city] * N
+                df = pd.DataFrame({
+                    "region_code": region_code_arr,
+                    "region_name": region_name_arr,
+                    "district_id": district_id_arr,
+                    "district_name": district_name_arr,
+                    "geohash": geohash_arr,
+                    "event_city": event_city_arr,
+                    "travel_city": travel_city_arr
+                })
+                file_name = f"geo_ref_{region.event_city}.csv"
+                new_path = os.path.join(directory, file_name)
+                df.to_csv(new_path)
+            except OSError:
+                continue
 
 
 if __name__ == "__main__":
--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,38 @@ import os.path
 from bs4 import BeautifulSoup
 import requests
 
+
+def parser(reg):
+    lin = reg.find('a').get('href')
+    new_l = "https://tools.paintmaps.com/" + lin
+    resp = requests.get(new_l).text
+    ms = BeautifulSoup(resp, 'html.parser')
+    block2 = ms.find_all('div', class_='col-md-12')[1]
+    li = block2.find('a').get('href')
+    newest_link = "https://tools.paintmaps.com/" + li
+    new_response2 = requests.get(newest_link).text
+    soup = BeautifulSoup(new_response2, 'html.parser')
+    bl = soup.find_all('div', class_='row')
+    bloc = ''
+    for el in bl:
+        if "(geojson format)" in el.text:
+            bloc = el
+    name_geo = bloc.find('a').text
+    geojson = bloc.find('a').get('href')
+    geo_url = "https://tools.paintmaps.com/" + geojson
+    geo_response = requests.get(geo_url)
+    if " " in name_geo:
+        name_geo = name_geo.replace(" ", "_")
+    if name_geo in regions_cnt:
+        regions_cnt[name_geo] = regions_cnt[name_geo] + 1
+        name_geo = name_geo[:-8] + str(regions_cnt[name_geo]) + ".geojson"
+    else:
+        regions_cnt[name_geo] = 1
+    with open(os.path.join(directory, name_geo), 'wb') as f:
+        f.write(geo_response.content)
+    print(f"Скачан файл: {name_geo}")
+
+
 link = "https://tools.paintmaps.com/map-cropping/RU"
 directory = 'geojsons_scrapped'
 if not os.path.exists(directory):
@@ -18,34 +50,12 @@ row_arr = big_block.find_all('div', class_='row')[5:]
 for r in row_arr:
     reg_3 = r.find_all('div', class_="col-md-4")
     for reg in reg_3:
-        lin = reg.find('a').get('href')
-        new_l = "https://tools.paintmaps.com/" + lin
-        resp = requests.get(new_l).text
-        ms = BeautifulSoup(resp, 'html.parser')
-        block2 = ms.find_all('div', class_='col-md-12')[1]
-        li = block2.find('a').get('href')
-        newest_link = "https://tools.paintmaps.com/" + li
-        new_response2 = requests.get(newest_link).text
-        soup = BeautifulSoup(new_response2, 'html.parser')
-        bl = soup.find_all('div', class_='row')
-        bloc = ''
-        for el in bl:
-            if "(geojson format)" in el.text:
-                bloc = el
-        name_geo = bloc.find('a').text
-        geojson = bloc.find('a').get('href')
-        geo_url = "https://tools.paintmaps.com/" + geojson
-        geo_response = requests.get(geo_url)
-        if name_geo in regions_cnt:
-            regions_cnt[name_geo] = regions_cnt[name_geo] + 1
-            name_geo = name_geo[:-8] + str(regions_cnt[name_geo]) + ".geojson"
-        else:
-            regions_cnt[name_geo] = 1
-        with open(os.path.join(directory, name_geo), 'wb') as f:
-            f.write(geo_response.content)
-        print(f"Скачан файл: {name_geo}")
+        if reg.find('a') is None:
+            continue
+        parser(reg)
     if "Zabaykalsky" in r.text:
         break
+
 
 block = main_soup.find('div', class_='panel-group')
 regions = block.find_all('div', class_='panel panel-default')
@@ -72,18 +82,8 @@ for reg in regions:
             geojson = bloc.find('a').get('href')
             geo_url = "https://tools.paintmaps.com/" + geojson
             geo_response = requests.get(geo_url)
-            if name_geo == "Nemeckij Nacional'nyj.geojson":
-                name_geo = "Nemeckij_Nacional'nyj.geojson"
-            if name_geo == "Uglegorsk (ZATO).geojson":
-                name_geo = "Uglegorsk_(ZATO).geojson"
-            if name_geo == "ZATO Mezhgor'e.geojson":
-                name_geo = "ZATO_Mezhgor'e.geojson"
-            if name_geo == "Dagestanskie Ogni.geojson":
-                name_geo = "Dagestanskie_Ogni.geojson"
-            if name_geo == "Franz Josef Land.geojson":
-                name_geo = "Franz_Josef_Land.geojson"
-            if name_geo == "Novaja Zemlja.geojson":
-                name_geo = "Novaja_Zemlja.geojson"
+            if " " in name_geo:
+                name_geo = name_geo.replace(" ", "_")
             if name_geo in regions_cnt:
                 regions_cnt[name_geo] = regions_cnt[name_geo] + 1
                 name_geo = name_geo[:-8] + str(regions_cnt[name_geo]) + ".geojson"
