41ce55d4eac578a24c0b5cc83b0799738666a8c5
DolmatovaKsenia <fantastic.drogon@gmail.com>
2025-02-25 12:36:23 +0300

    Update result_union.py

--- result_union.py
+++ result_union.py
@@ -1,15 +1,23 @@
 import os
+import pandas as pd
 
-filenames = []
+dataframes = []
 
 for fname in os.listdir("final_regions_csv"):
     if fname.endswith('.csv'):
-        filenames.append(fname)
+        data = pd.read_csv(os.path.join("final_regions_csv", fname), sep=',')
+        dataframes.append(data)
 
-with open('result_union_file.csv', 'w', newline='') as outfile:
-    for fname in filenames:
-        with open(os.path.join("final_regions_csv", fname), 'r') as infile:
-            for line in infile:
-                outfile.write(line)
+big_data = pd.concat(dataframes, ignore_index=True)
+
+if "Unnamed: 0" in big_data.columns:
+    big_data = big_data.drop("Unnamed: 0", axis=1)
+
+big_data.reset_index(drop=False, inplace=True)
+big_data.rename(columns={'index': 'row_number'}, inplace=True)
+
+shablon = ["row_number", "region_code", "region_name", "district_id", "district_name", "geohash", "event_city", "travel_city"]
+
+big_data.to_csv("result_union_file.csv", sep=',', index=False)
 
 print("Данные успешно записаны в result_union_file.csv")
